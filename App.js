8afcd97ae3f8e79568f3a5a4905d8b0a6d343e9f
Caleb Nance <calebnance@gmail.com>
2018-10-03 12:40:12 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>welcome!</Text>
+        <Text style={styles.text}>
+          this is a simple one page react native app
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#1d2d3a",
+    flex: 1,
+    justifyContent: "center"
+  },
+  text: {
+    color: "#fff"
+  }
+});
