90a5c64206e4eac83027bded9006c49cd14fc8ad
Caleb Nance <calebnance@gmail.com>
2018-10-04 12:40:12 -0400

    is iPhoneX, iPhoneXs, iPhoneXr, iPhoneXs Max added

---
+++ src/api/device.js
@@ -0,0 +1,22 @@
+import { Dimensions, Platform } from 'react-native';
+
+// device
+// /////////////////////////////////////////////
+
+// is iPhoneX, iPhoneXs, iPhoneXr, iPhoneXs Max
+const dims = Dimensions.get('window');
+const { height, width } = dims;
+
+let iPhoneX = false;
+if (
+  Platform.OS === 'ios' &&
+  (height === 812 || width === 812 || (height === 896 || width === 896))
+) {
+  // iphone screen breakdown
+  // https://www.paintcodeapp.com/news/ultimate-guide-to-iphone-resolutions
+  iPhoneX = true;
+}
+
+export default {
+  iPhoneX
+};
