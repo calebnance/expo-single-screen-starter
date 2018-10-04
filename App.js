f1bb8aae14a6f6e0a86367e7aec841e238f6be93
Caleb Nance <calebnance@gmail.com>
2018-10-04 15:25:14 -0400

    menu screen added and a little functionality for it

--- App.js
+++ App.js
@@ -2,27 +2,62 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { colors } from './src/api/constants';
 
+// screens
+import Menu from './src/screens/Menu';
+
 // component(s)
 import Touch from './src/components/Touch';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showLeftMenu: false,
+      showRightMenu: false
+    };
+
+    this.menuOpen = this.menuOpen.bind(this);
+    this.menuClose = this.menuClose.bind(this);
+  }
+
+  menuOpen(showMenu) {
+    this.setState({
+      showLeftMenu: showMenu === 'showLeftMenu',
+      showRightMenu: showMenu === 'showRightMenu'
+    });
+  }
+
+  menuClose() {
+    this.setState({
+      showLeftMenu: false,
+      showRightMenu: false
+    });
+  }
+
   render() {
+    const { showLeftMenu, showRightMenu } = this.state;
+
     return (
       <View style={styles.container}>
         <Text style={styles.text}>welcome!</Text>
         <Text style={styles.text}>
           this is a simple one page react native app
         </Text>
+
         <Touch
           style={[styles.block, styles.leftBlock]}
           text="left"
-          onPress={() => console.log('bottom left block')}
+          onPress={() => this.menuOpen('showLeftMenu')}
         />
         <Touch
           style={[styles.block, styles.rightBlock]}
           text="right"
-          onPress={() => console.log('bottom right block')}
+          onPress={() => this.menuOpen('showRightMenu')}
         />
+
+        <Menu show={showLeftMenu} onClose={this.menuClose} />
+        <Menu direction="right" show={showRightMenu} onClose={this.menuClose} />
       </View>
     );
   }
--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,44 +1,113 @@
 import React from 'react';
-import { Animated } from 'react-native';
-import { device } from '../api/constants';
+import { Animated, ScrollView, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { colors, device } from '../api/constants';
+
+// component(s)
+import Touch from '../components/Touch';
 
 class Menu extends React.Component {
   constructor(props) {
     super(props);
 
+    let startPos = -device.width;
+    if (props.direction === 'right') {
+      startPos = device.width;
+    }
+
     this.state = {
-      position: new Animated.Value(-device.width)
+      position: new Animated.Value(startPos)
     };
   }
 
   componentDidMount() {
-    // console.log('componentDidMount()');
+    console.log('Menu::componentDidMount()');
+  }
 
+  componentDidUnMount() {
+    console.log('Menu::componentDidUnMount()');
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log('Menu::componentDidUpdate()');
+
+    const { show } = this.props;
     const { position } = this.state;
-
-    Animated.timing(position, {
-      toValue: 0,
-      delay: 400,
-      duration: 400
-    }).start();
+    if (show !== prevProps.show) {
+      Animated.timing(position, {
+        toValue: 0,
+        // delay: 400,
+        duration: 400
+      }).start();
+    }
   }
 
   render() {
+    const { show } = this.props;
     const { position } = this.state;
+
+    if (show === false) {
+      return null;
+    }
 
     return (
       <Animated.View
         style={{
-          backgroundColor: 'red',
+          height: '100%',
           left: position,
           position: 'absolute',
-          height: '100%',
-          width: '80%',
+          width: '100%',
           zIndex: 100
         }}
-      />
+      >
+        <View
+          style={{
+            backgroundColor: colors.white50,
+            alignItems: 'stretch',
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            height: '100%',
+            width: '100%'
+          }}
+        >
+          <ScrollView
+            style={{
+              backgroundColor: 'blue',
+              height: '100%',
+              width: '80%'
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: colors.white,
+                height: 75,
+                width: '100%'
+              }}
+            />
+          </ScrollView>
+          <Touch
+            onPress={() => this.props.onClose()}
+            style={{
+              height: '100%',
+              width: '20%'
+            }}
+          />
+        </View>
+      </Animated.View>
     );
   }
 }
+
+// default props
+Menu.defaultProps = {
+  direction: 'left'
+};
+
+// type checking
+Menu.propTypes = {
+  // optional
+  direction: PropTypes.string
+};
 
 export default Menu;
