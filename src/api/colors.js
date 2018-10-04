fca871ca32b06cb7865ed4b74acb2504edac228a
Caleb Nance <calebnance@gmail.com>
2018-10-04 12:39:48 -0400

    colors and utilStyles added

---
+++ src/api/colors.js
@@ -0,0 +1,15 @@
+// colors
+// /////////////////////////////////////////////
+export default {
+  black: '#000',
+  black50: 'rgba(0, 0, 0, 0.5)',
+  white: '#fff',
+  white50: 'rgba(255, 255, 255, 0.5)',
+
+  brandPrimary: '#1d2d3a',
+
+  blue: '#0197f6',
+  brown: '#bcab79',
+  grey: '#d0ccd0',
+  red: '#d7263d'
+};
---
+++ src/api/utilStyles.js
@@ -0,0 +1,6 @@
+// utility styles
+// /////////////////////////////////////////////
+export default {
+  opacityActive: 0.8,
+  opacityActiveFull: 1
+};
