988ad435e2907b46ceadec76b78614a593d51d61
Caleb Nance <calebnance@gmail.com>
2019-04-06 23:50:03 -0400

    moved tests location

--- src/components/__tests__/Touch.js
+++ src/components/__tests__/Touch.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import Touch from '../../components/Touch';
+import Touch from '../Touch';
 
 describe('<Touch />', () => {
   it('renders correctly', () => {
--- src/screens/__tests__/Menu.js
+++ src/screens/__tests__/Menu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import Menu from '../../screens/Menu';
+import Menu from '../Menu';
 
 describe('<Menu />', () => {
   beforeEach(() => {
