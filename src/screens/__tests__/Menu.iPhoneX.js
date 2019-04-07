db3b4e78304e5c87511d72b7d070025a10e5de58
Caleb Nance <calebnance@gmail.com>
2019-04-07 01:25:58 -0400

    Dimensions Mocking with Jest

---
+++ src/screens/__tests__/Menu.iPhoneX.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Menu from '../Menu';
+import Touch from '../../components/Touch';
+
+// jest.mock('Platform', () => {
+//   const Platform = jest.requireActual('Platform');
+//   Platform.OS = 'ios';
+//
+//   return Platform;
+// });
+//
+
+jest.mock('Dimensions', () => {
+  const Dimensions = jest.requireActual('Dimensions');
+  Dimensions.get = jest.fn().mockReturnValue({ height: 896, width: 812 });
+
+  return Dimensions;
+});
+
+describe('<Menu />', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  it('is hidden', () => {
+    const tree = renderer.create(<Menu onClose={jest.fn()} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly on left', () => {
+    const tree = renderer.create(<Menu onClose={jest.fn()} show />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly on right', () => {
+    const tree = renderer
+      .create(<Menu direction="right" onClose={jest.fn()} show />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('closes menu', () => {
+    jest.useFakeTimers();
+
+    const testRenderer = renderer.create(<Menu onClose={jest.fn()} show />);
+    const testInstance = testRenderer.root;
+    const touchCloseArea = testInstance.findByType(Touch);
+    touchCloseArea.props.onPress();
+  });
+});
